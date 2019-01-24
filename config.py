df728a1457db3137c2e19358d001c9b66078b06e
Chris Sheldrick <csheldrick@gmail.com>
2019-01-24 17:25:28 -0500

    fix broken loading

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-users = {"264": "csheldrick", "2807": "teccles"}
-leaderboard_interval = 5000
-finals_interval = 5000
-game_feed_interval = 5000
+users = {"264": "csheldrick", "9475": "ubahka", "9514": "xantiox"}
+leaderboard_interval = 10000
+finals_interval = 15000
+game_feed_interval = 15000
